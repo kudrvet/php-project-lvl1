5dd3bb6ca019b1bfd5205a4cf4454e8f11085ca4
Vitaliy <kudrvet@gmail.com>
2020-05-03 14:42:43 +0300

    code refactor

--- src/games/PrimeGame.php
+++ src/games/PrimeGame.php
@@ -2,6 +2,10 @@
 
 namespace BrainGames\Games\PrimeGame;
 
+use function BrainGames\Flow\finishGame;
+use function BrainGames\Flow\greetingUser;
+use function BrainGames\Flow\isAnswerRightAndConsoleOutput;
+
 function getRightAnswerOfPrimeGame($number)
 {
     for ($i = 2; $i <= $number / 2; $i++) {
@@ -11,4 +15,46 @@ function getRightAnswerOfPrimeGame($number)
     }
 
     return "yes";
+}
+
+function generateBrainPrimeData()
+{
+    $gameRule = "Answer \"yes\" if given number is prime. Otherwise answer \"no\".";
+
+    $number = rand(0, 3000);
+    $rightAnswer = getRightAnswerOfPrimeGame($number);
+    $question = $number;
+
+
+    return [$gameRule,$question,$rightAnswer];
+}
+
+function startGame()
+{
+    $gameData = generateData();
+    $gameRule = $gameData[0];
+    $userName = greetingUser($gameRule);
+    $countOfCorrectAnswers = 0;
+    $needfulCountOfCorrectAnswers = 3;
+    while ($countOfCorrectAnswers !== $needfulCountOfCorrectAnswers) {
+        $gameData = generateData();
+        $isUserRight = isAnswerRightAndConsoleOutput($gameData, $userName);
+        if ($isUserRight) {
+            $countOfCorrectAnswers += 1;
+        } else {
+            $countOfCorrectAnswers = 0;
+        }
+    }
+    finishGame($userName);
+}
+
+function generateData()
+{
+    $gameData = generateBrainPrimeData();
+    return $gameData;
+}
+
+function startBrainPrime()
+{
+    startGame();
 }
